497ee4ef0aae03040943dac31f43158c86ba9c37
MerlinKegler <mrmoerlin@gmail.com>
2019-03-08 00:30:54 +0100

    testcommit

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 const express = require('express')
 const app = express()
 
-app.use('/static', express.static('public'))
-app.use('/imgs', express.static('imgs'))
+// app.use('/static', express.static('public'))
+// app.use('/imgs', express.static('imgs'))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
