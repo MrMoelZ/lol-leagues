c5ac42ce6ffdbb155ee7ce55485f4a66435f5bd0
MerlinKegler <mrmoerlin@gmail.com>
2019-03-08 00:36:48 +0100

    reverted test

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 const express = require('express')
 const app = express()
 
-// app.use('/static', express.static('public'))
-// app.use('/imgs', express.static('imgs'))
+app.use('/static', express.static('public'))
+app.use('/imgs', express.static('imgs'))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
@@ -12,3 +12,5 @@ app.get('/', (req, res) => {
 app.listen(3003, () => {
     console.log('listening on 3003')
 })
+
+//loldatabase:europe-west1:myinstance
